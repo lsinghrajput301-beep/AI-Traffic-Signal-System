4b51f1d4a30fa284fe17c428b6b8e114bf782504
lokendra singh <lsinghrajput301@gmail.com>
2026-09-03 14:25:32 +0530

    Add traffic signal logic for four roads
    Implement traffic signal timing based on vehicle count.

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-AI-Traffic-Signal-System
-│
-├── README.md
-├── main.py
-└── requirements.txt
+print("AI Traffic Signal System")
+
+road_A = int(input("Road A par vehicles: "))
+road_B = int(input("Road B par vehicles: "))
+road_C = int(input("Road C par vehicles: "))
+road_D = int(input("Road D par vehicles: "))
+
+traffic = {
+    "Road A": road_A,
+    "Road B": road_B,
+    "Road C": road_C,
+    "Road D": road_D
+}
+
+for road, vehicles in traffic.items():
+
+    if vehicles >= 30:
+        green_time = 60
+    elif vehicles >= 20:
+        green_time = 40
+    elif vehicles >= 10:
+        green_time = 30
+    else:
+        green_time = 20
+
+    print(road, "-> GREEN", green_time, "seconds")
